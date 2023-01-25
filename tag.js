99156ed29bf4e9e27e89ca419286f8f968662344
retroinspect <sangachoi@kaist.ac.kr>
2023-01-25 19:37:14 +0900

    Add tag checker

---
+++ tag.js
@@ -0,0 +1,15 @@
+const { version } = require('./package.json')
+
+const core = require('@actions/core');
+
+const tagVersion = process.env.GITHUB_REF_NAME
+
+console.log(`package.json version: ${version}`)
+console.log(`tagVersion: ${tagVersion}`)
+
+core.notice(`package.json version: ${version}`)
+core.notice(`tagVersion: ${tagVersion}`)
+
+if (version !== tagVersion) {
+    core.setFailed(`Action failed since tag version and package.json version are different`)
+}
